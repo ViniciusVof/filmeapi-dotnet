b36c983e47788f407f3fdcf19b0347cd6bf811fb
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2023-10-03 23:05:26 -0300

    Configurando camada de banco

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using webapi_dotnet.Models;
+
+namespace webapi_dotnet.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opts) 
+            : base(opts) 
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
